a8e10e8cf977d7f547c45f2dedf2ada7720d5cb4
Lisa-Stubert <lisastubert@web.de>
2024-08-16 17:12:20 +0200

    fix: typescript errors, delete unused variables

--- src/components/radar/range-legend.tsx
+++ src/components/radar/range-legend.tsx
@@ -1,18 +1,18 @@
 import { ranges } from "../../data/ranges";
 import { RADAR_WIDTH } from "../../geometry/constants";
 
-const paddingMapping: Record<string, string> = {
-	"0": "18",
-	"1": "38",
-	"2": "23",
-	"3": "18",
-};
+// const paddingMapping: Record<string, string> = {
+// 	"0": "18",
+// 	"1": "38",
+// 	"2": "23",
+// 	"3": "18",
+// };
 
 export function RangeLegend() {
 	return (
 		<div className="absolute right-0 h-full" style={{ width: RADAR_WIDTH / 2 }}>
 			<div className="text-xs flex h-full items-center w-full -mt-5">
-				{ranges.map((range, index) => (
+				{ranges.map((range) => (
 					<span
 						key={range.name}
 						className="text-sr-lighter-black"
